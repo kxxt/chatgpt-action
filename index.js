10ebbdaa6c737e078fddb00a0b9ea0359a61063e
kxxt <rsworktech@outlook.com>
2022-12-07 22:11:41 +0800

    remove unused func

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ async function callChatGPT(api, content) {
   return response;
 }
 
-function genCommentPRPrompt(title, body) {
-  return `Here is a pull request, please comment:\n
-title: ${title}
-body: ${body}
-changes: `;
-}
-
 function genReviewPRPrompt(title, body, diff) {
   const prompt = `Can you tell me the problems with the following pull request and describe your suggestions? 
 title: ${title}
