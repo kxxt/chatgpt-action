8a7e8b6b942928ca364aef7b1bebd5798c26284b
kxxt <rsworktech@outlook.com>
2022-12-07 21:37:17 +0800

    update prompt

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ changes: `;
 }
 
 function genReviewPRPrompt(title, body, diff) {
-  const prompt = `Can you tell me the problems with the following pull request and your suggestions?
+  const prompt = `Can you tell me the problems with the following pull request and describe your suggestions? 
 title: ${title}
 body: ${body}
 The following diff is the changes made in this PR.
