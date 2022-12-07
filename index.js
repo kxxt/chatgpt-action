096327845cb1646697c38025e0ee95c04f9fc81f
kxxt <rsworktech@outlook.com>
2022-12-07 19:54:51 +0800

    add mode option

--- index.js
+++ index.js
@@ -24,38 +24,52 @@ async function callChatGPT(api, content) {
 function genCommentPRPrompt(title, body) {
   return `Here is a pull request, please comment:\n
 title: ${title}
-body: ${body}`;
+body: ${body}
+changes: `;
 }
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const context = github.context;
-    const pr = parseInt(core.getInput("pr"));
+    const number = parseInt(core.getInput("number"));
     const sessionToken = core.getInput("sessionToken");
+    const mode = core.getInput("mode");
 
     // Read PR title and body
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    const {
-      data: { title, body },
-    } = await octokit.pulls.get({
-      owner,
-      repo,
-      pull_number: pr,
-    });
-    core.info(`title: ${title}`);
-    core.info(`body:  ${body}`);
+
     // Create ChatGPT API
     const api = await createChatGPTAPI(sessionToken);
 
-    const response = await callChatGPT(api, genCommentPRPrompt(title, body));
-    core.setOutput("comment", response);
+    if (mode == "pr") {
+      const {
+        data: { title, body },
+      } = await octokit.pulls.get({
+        owner,
+        repo,
+        pull_number: number,
+      });
+      const response = await callChatGPT(api, genCommentPRPrompt(title, body));
+      core.setOutput("comment", response);
 
-    await octokit.issues.createComment({
-      ...context.repo,
-      issue_number: pr,
-      body: response,
-    });
+      await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: number,
+        body: response,
+      });
+    } else if (mode == "issue") {
+    } else if (mode == "review") {
+      const patch = await octokit.request(
+        "GET /{owner}/{repo}/pull/{number}.patch",
+        {
+          owner,
+          repo,
+          number,
+        }
+      );
+      core.info(patch);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
